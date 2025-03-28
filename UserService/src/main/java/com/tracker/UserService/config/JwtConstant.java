6f25b782b667e943357682f894826b04f40928d6
Anna <trembach.ann@gmail.com>
2025-03-28 17:34:57 +0200

    added transaction service

--- UserService/src/main/java/com/tracker/UserService/config/JwtConstant.java
+++ UserService/src/main/java/com/tracker/UserService/config/JwtConstant.java
@@ -2,5 +2,4 @@ package com.tracker.UserService.config;
 
 public class JwtConstant {
     public static final String SECRET_KEY = "rvbirbwivbiwbvwisvksvsevekvrbrierv";
-    public static final String JWT_HEADER = "Authorization";
 }
