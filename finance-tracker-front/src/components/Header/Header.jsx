5e72fb4b0cf0279094ed5186803cfb0752f8bd51
Anna <trembach.ann@gmail.com>
2025-04-30 22:25:38 +0300

    added little chenges in dashboard

--- finance-tracker-front/src/components/Header/Header.jsx
+++ finance-tracker-front/src/components/Header/Header.jsx
@@ -1,47 +1,59 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css';
 import './HeaderStyle.css';
 
 function Header({ setIsSidebarOpen }) {
+  const sidenavRef = useRef(null);
+  const sidenavInstanceRef = useRef(null);
+
   useEffect(() => {
-    const elems = document.querySelectorAll('.sidenav');
-    const instances = M.Sidenav.init(elems, {
+    const instance = M.Sidenav.init(sidenavRef.current, {
       onOpenStart: () => setIsSidebarOpen(true),
       onCloseEnd: () => setIsSidebarOpen(false),
     });
+    sidenavInstanceRef.current = instance;
 
     return () => {
-      instances.forEach(instance => instance.destroy());
+      instance.destroy();
     };
   }, [setIsSidebarOpen]);
+
+  const handleBurgerClick = () => {
+    if (sidenavInstanceRef.current) {
+      if (sidenavInstanceRef.current.isOpen) {
+        sidenavInstanceRef.current.close();
+      } else {
+        sidenavInstanceRef.current.open();
+      }
+    }
+  };
 
   return (
     <div className="navbar-fixed">
       <nav>
         <div className="nav-wrapper">
-          {/* Підключення іконок Material Icons */}
           <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
           
-          {/* Бургер-меню */}
-          <a href="#" data-target="slide-out" className="sidenav-trigger show-burger">
+          <a onClick={handleBurgerClick} className="show-burger">
             <i className="material-icons">menu</i>
           </a>
 
-          {/* Логотип */}
           <a href="/" className="brand-logo" style={{ marginLeft: "60px" }}>
             Finance Tracker
           </a>
 
-          {/* Правий кут (username) */}
           <ul id="nav-mobile" className="right hide-on-med-and-down">
             <li>username</li>
           </ul>
         </div>
       </nav>
 
-      {/* Сайдбар */}
-      <ul id="slide-out" className="sidenav">
+      <ul
+        id="slide-out"
+        className={`sidenav ${sidenavInstanceRef.current && sidenavInstanceRef.current.isOpen ? 'open' : 'close'}`}
+        ref={sidenavRef}
+      >
         <li><a href="/">Second Link</a></li>
       </ul>
     </div>
